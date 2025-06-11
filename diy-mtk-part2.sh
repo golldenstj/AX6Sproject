1a1934071daf1f3f6a8c0d88c6f3f06653214282
golldenstj <70574688+golldenstj@users.noreply.github.com>
2025-06-11 17:55:13 +0800

    Update diy-mtk-part2.sh

--- diy-mtk-part2.sh
+++ diy-mtk-part2.sh
@@ -14,6 +14,10 @@
 sed -i 's#192.168.1.1#192.168.0.1#g' package/base-files/files/bin/config_generate
 
 # Rust 编译器参数改成 download-ci-llvm = false，自己编译 LLVM
+# 删除 download-ci-llvm 的设置（无论 true 或 if-unchanged）
 sed -i 's/--set=llvm\.download-ci-llvm=.*\\/--set=llvm.download-ci-llvm=false \\/' feeds/packages/lang/rust/Makefile
-sed -i '/--set=llvm.download-ci-llvm=false \\/a \ \ \ \ --set=llvm.build=true \\' feeds/packages/lang/rust/Makefile
+# 添加 --set=llvm.build=true 到其下一行
+sed -i '/--set=llvm\.download-ci-llvm=false \\/a \ \ \ \ --set=llvm.build=true \\' feeds/packages/lang/rust/Makefile
+# 删除已废弃且报错的 bootstrap-cache-path
+sed -i '/--set=build\.bootstrap-cache-path=/d' feeds/packages/lang/rust/Makefile
 

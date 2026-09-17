8b83d783b0fd59f7c5c928f7b7717bc668078f13
golldenstj <70574688+golldenstj@users.noreply.github.com>
2026-09-17 15:03:32 +0800

    Update diy-mtk-part1.sh

--- diy-mtk-part1.sh
+++ diy-mtk-part1.sh
@@ -15,6 +15,6 @@
 
 # Add a feed source
 #sed -i "/helloworld/d" "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+#echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 echo "src-git PWpackages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git" >> "feeds.conf.default"
 echo "src-git PWluci https://github.com/Openwrt-Passwall/openwrt-passwall.git" >> "feeds.conf.default"
